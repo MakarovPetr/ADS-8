944bfdf9f6bece51d922bf1d8cbad3497e8d7344
MakarovPetr <125215189+MakarovPetr@users.noreply.github.com>
2023-05-13 14:11:55 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,61 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() : first(nullptr), countOp(0) {}
+
+void Train::addCage(bool light) {
+    if (first == nullptr) {
+        first = new Cage;
+        first->prev = nullptr;
+        first->next = nullptr;
+        first->light = true;
+    }
+    else if (first != nullptr) {
+        Cage* cage = new Cage;
+        cage->next = nullptr;
+        cage->prev = nullptr;
+        cage->light = light;
+        Cage* tmp;
+        if (first->next == nullptr) {
+            cage->next = first;
+            cage->prev = first;
+            first->next = cage;
+            first->prev = cage;
+        } else {
+            tmp = first;
+            while (tmp->next != first) {
+                tmp = tmp->next;
+            }
+            tmp->next = cage;
+            cage->next = first;
+            cage->prev = tmp;
+            first->prev = cage;
+        }
+    }
+}
+
+int Train::getLength() {
+    Cage* tmp = first;
+    int count = 0;
+    while (tmp) {
+        countOp++;
+        tmp = tmp->next;
+        if (tmp->light == 0) {
+            count++;
+            //countOp++;
+        }
+        if (tmp->light == 1) {
+            tmp->light = 0;
+            for (int i = count; i > 0; i--) {
+                countOp++;
+                tmp = tmp->prev;
+            }
+            if (tmp->light == 0)
+                return count + 1;
+        }
+    }
+}
+
+int Train::getOpCount() {
+    return countOp + 1;
+}
